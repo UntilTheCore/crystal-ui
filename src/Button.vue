71580116d4f2dd254232af1cc316cd76f0a3ab21
UntilTheCore <814898862@qq.com>
2020-10-03 15:29:03 +0800

    可设置icon所在位置；增加props属性验证

--- src/Button.vue
+++ src/Button.vue
@@ -1,20 +1,40 @@
 <template>
-  <button>
-    <slot />
+  <button :class="{[`icon-${iconPosition}`]:true}">
+    <svg v-if="iconName" class="icon" aria-hidden="true">
+      <use :xlink:href=`#icon-${iconName}`></use>
+    </svg>
+    <span class="content"> <slot /> </span>
   </button>
 </template>
 <script lang="js">
-export default {}
+export default {
+  props: {
+    iconName: String,
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator(value) {
+        return value === 'left' || value === 'right'
+      }
+
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-.c-button {
-  font-style    : var(--font-size);
-  height        : var(--button-height);
-  padding       : 0 1em;
-  border-radius : var(--border-radius);
-  border        : 1px solid var(--border-color);
-  background    : var(--button-bg);
+button {
+  display        : inline-flex;
+  align-items    : center;
+  font-style     : var(--font-size);
+  height         : var(--button-height);
+  padding        : 0 1em;
+  border-radius  : var(--border-radius);
+  border         : 1px solid var(--border-color);
+  background     : var(--button-bg);
+  outline        : none;
+  line-height    : 1;
+  vertical-align : middle;
 
   &:hover {
     background-color : var(--border-color-hover);
@@ -22,6 +42,28 @@ export default {}
 
   &:active {
     background-color : var(--button-active-bg);
+  }
+
+  > svg {
+    order        : 1;
+    margin-right : .2em;
+  }
+
+  > .content {
+    order          : 2;
+    vertical-align : middle;
+  }
+
+  &.icon-right {
+    > svg {
+      order        : 2;
+      margin-right : 0;
+      margin-left  : .2em;
+    }
+
+    > .content {
+      order : 1;
+    }
   }
 }
 
